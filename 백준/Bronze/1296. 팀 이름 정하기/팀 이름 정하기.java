c3caddfb144b74b0d42838fae48aed91e674afbd
KIM TAE YEOP <108780666+tony99kim@users.noreply.github.com>
2026-03-27 22:54:54 +0900

    [Bronze I] Title: 팀 이름 정하기, Time: 176 ms, Memory: 17848 KB -BaekjoonHub

---
+++ 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+ 
+        String yeondu =sc.nextLine();
+        int n = sc.nextInt();
+        sc.nextLine();
+        
+        String[] teams = new String[n];
+        for(int i=0; i<n; i++){
+            teams[i] = sc.nextLine();
+        }
+        
+        int L=0,O=0,V=0,E=0;
+        for(char c : yeondu.toCharArray()){
+            if(c == 'L') L++;
+            else if(c == 'O')O++;
+            else if(c == 'V')V++;
+            else if(c == 'E')E++;
+        }
+        
+        String answer = "";
+        int max= -1;
+        
+        for(String team : teams){
+            int l=L,o=O,v=V,e=E;
+            for(char c : team.toCharArray()){
+                if(c == 'L') l++;
+                else if(c == 'O')o++;
+                else if(c == 'V')v++;
+                else if(c == 'E')e++;
+            }
+            int score =(l+o)*(l+v)*(l+e)*(o+v)*(o+e)*(v+e)%100;
+            
+            if(score>max){
+                max = score;
+                answer = team;
+            }else if (score == max){
+                if(team.compareTo(answer)<0){
+                    answer = team;
+                }
+            }             
+        }
+        System.out.print(answer);
+    }
+}
